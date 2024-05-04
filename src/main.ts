1a7804a69ed7f4b575a8def26bd2c00efee218d1
grGrimales <grojasgrimales@gmail.com>
2024-05-03 21:44:05 -0300

    add auth

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+
+  dotenv.config();
+
+
+  const app = await NestFactory.create(AppModule);
+    // Enable CORS
+    const corsOptions = {
+      origin: ['http://localhost:4200', 'http://localhost:3000', 'https://listen-with-me-app.vercel.app'],
+      credentials: true,
+    };
+
+  app.enableCors(
+    corsOptions,
+  );
+  // Enable Validation
+    app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
+}
+bootstrap();
